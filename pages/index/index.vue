0048114d3760ea218822c6cf7256c77e9c4beadc
hed <comeonhd@sina.com>
2019-01-13 12:17:10 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="content">
+        <image class="logo" src="../../static/logo.png"></image>
+		<view>
+            <text class="title">{{title}}</text>
+        </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 300upx;
+	}
+    .logo{
+        height: 60upx;
+        width: 60upx;
+        margin-top: 50upx;
+    }
+	.title {
+		font-size: 24upx;
+		color: #8f8f94;
+	}
+</style>
